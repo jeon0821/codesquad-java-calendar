58c6f758df288f052d90aef02db38518fe7c4b54
jieun <plaintype@gmail.com>
2023-04-10 21:36:06 +0900

    Update Leap Year

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -5,9 +5,21 @@ import java.util.Scanner;
 public class Calendar {
 
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
+	}
+	
+	public int getMaxDaysOfMonth(int year, int month) {
+		if(isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}		
 	}
 
 	public void printCalendar(int year, int month) {
@@ -15,7 +27,7 @@ public class Calendar {
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
 		
-		int maxDay = getMaxDaysOfMonth(month);
+		int maxDay = getMaxDaysOfMonth(year, month);
 		
 		for(int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d",i);
@@ -28,36 +40,6 @@ public class Calendar {
 //		System.out.println("8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
-	}
-
-	public static void main(String[] args) {
-		
-		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		
-		String PROMPT="cal>";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-		
-		int month = 1;
-		
-		while(true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if(month == -1) { // -1이면 루프를 빠져나가는 것.
-				break;
-			}
-			
-			if(month > 12) {
-				continue; // continue는 만나면 루프의 처음으로 돌아감.
-			}
-			
-			System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month)); // 배열의 인덱스는 0부터 시작이라서
-
-		}
-		
-		System.out.println("Bye~");
-		scanner.close();
 	}
 
 }
