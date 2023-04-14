335c35f8d7d1cc70cb42e6ac557185b3c60c5f87
jieun <plaintype@gmail.com>
2023-04-14 23:23:13 +0900

    Add Help

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class Prompt {
 	
+	public void printMenu() {
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말 q. 종료");
+		System.out.println("+----------------------+");
+	}
+	
 	/**
 	 * 
 	 * @param week 요일명
@@ -22,35 +31,62 @@ public class Prompt {
 			return 0;
 		
 	}
+	
+//	+----------------------+
+//	| 1. 일정 등록           
+//	| 2. 일정 검색           
+//	| 3. 달력 보기
+//	| h. 도움말 q. 종료
+//	+----------------------+
+//  명령 (1, 2, 3, h, q)
 
 	public void runPrompt() {
+		printMenu();
+		
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
+		while (true) {
+			System.out.println("명령 (1, 2, 3, h, q)");
+			String cmd = scanner.next();
+			if(cmd.equals("1")) cmdRegister();
+			else if (cmd.equals("2")) cmdSearch();
+			else if (cmd.equals("3")) cmdCal(scanner, cal);
+			else if (cmd.equals("h")) printMenu();
+			else if (cmd.equals("q")) break;
+		}
+			
+		System.out.println("Thank you. Bye~");
+		scanner.close();
+	}
+		
+	private void cmdCal(Scanner s, Calendar c) {
 		int month = 1;
 		int year = 2017;
+		System.out.println("년도를 입력하세요.(exit: -1)");
+		System.out.print("YEAR> ");
+		year = s.nextInt();
+		
+		System.out.println("달을 입력하세요.");
+		System.out.print("MONTH> ");
+		month = s.nextInt();
 
-		while (true) {
-			System.out.println("년도를 입력하세요.(exit: -1)");
-			System.out.print("YEAR> ");
-			year = scanner.nextInt();
-			if(year == -1)
-				break;
-			
-			System.out.println("달을 입력하세요.");
-			System.out.print("MONTH> ");
-			month = scanner.nextInt();
-
-			if (month > 12 || month < 1) {
-				System.out.println("잘못된 입력입니다.");
-				continue;
-			}
-
-			cal.printCalendar(year, month);
+		if (month > 12 || month < 1) {
+			System.out.println("잘못된 입력입니다.");
+			return;
 		}
 
-		System.out.println("Bye~");
-		scanner.close();
+		c.printCalendar(year, month);
+	}
+
+	private void cmdSearch() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void cmdRegister() {
+	// TODO Auto-generated method stub
+
 	}
 
 	public static void main(String[] args) {
